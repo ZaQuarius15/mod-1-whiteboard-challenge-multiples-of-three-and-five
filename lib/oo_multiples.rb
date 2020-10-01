294694ebc1baa34a553f946001cc022c53b5ac38
ZaQuarius15 <jzcarlson014@gmail.com>
2020-10-01 09:54:16 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+    attr_reader
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (1...@limit).select do |n|
+            (n % 3) == 0 || (n % 5) == 0
+        end
+    end 
+
+    def sum_multiples
+        self.collect_multiples.sum
+    end
+
+
+end
